cf2a8df29acd972de8738eb94e56d6e170cb17f1
wyekstudent <e1527473@u.nus.edu>
2026-08-18 18:25:44 +0800

    Code cleanup
    Used constants for max tasks and seperator

--- src/main/java/Dingleberry.java
+++ src/main/java/Dingleberry.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Dingleberry {
+    private static final int MAX_TASKS = 100;
+    private static final String SEPARATOR = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner =
                 """
@@ -13,36 +16,31 @@ public class Dingleberry {
                                        |___/                              |___|
                         
                         Hey There! I'm Dingleberry
-                        Whata cana I doa for you?
+                        What can I do for you?
                         ____________________________________________________________
                         """;
-        // Prints out above banner
         System.out.println(banner);
 
-        // Takes in user input
         Scanner scanner = new Scanner(System.in);
         int count = 0;
-        int maxTasks = 100; // Max number of tasks
-        String[] dinglelist = new String[maxTasks];
+        String[] dinglelist = new String[MAX_TASKS];
 
-        while(count < maxTasks) {
+        while(count < MAX_TASKS) {
             String input = scanner.nextLine();
 
-            // Make it case-insensitive
             if (input.equalsIgnoreCase("bye")) {
                 break;
             } else if (input.equalsIgnoreCase("list")) {
-                // Sets the list item
-                System.out.println("____________________________________________________________");
+                System.out.println(SEPARATOR);
                 for (int i = 1; i <= count; i++) {
                     System.out.printf("%d. %s\n", i, dinglelist[i - 1]);
                 }
-                System.out.println("____________________________________________________________");
+                System.out.println(SEPARATOR);
             } else {
                 dinglelist[count++] = input;
-                System.out.println("____________________________________________________________");
+                System.out.println(SEPARATOR);
                 System.out.println("added: " + input);
-                System.out.println("____________________________________________________________");
+                System.out.println(SEPARATOR);
             }
         }
         scanner.close();
